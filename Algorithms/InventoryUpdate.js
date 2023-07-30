981ede9cceb6e19618f552e208e3583e9a61a667
Billy Torres <btorresr75@gmail.com>
2023-07-30 11:25:39 -0600

    Cambios en el algoritmo

--- Algorithms/InventoryUpdate.js
+++ Algorithms/InventoryUpdate.js
@@ -2,31 +2,27 @@
 // Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
 
 function updateInventory(arr1, arr2) {
-    const rows = arr1.length
-    const cols = arr1[0].length
-    const newArr = []
-    
-    for(let x = 0; x < rows; x++) {
-        for(let y = 0; y < cols; y++) {
-            let elementExist = arr1[x][1] === arr2[y][1]
-            if(elementExist) {
-                newArr.push(arr2[y])
-            } else {
-                newArr.push(arr1[x])
-            }
+    const newArr = []    
+    for(let i = 0; i < arr1.length; i++) {
+        for(let j = 0; j < arr1.length; j++) {
+            let expresion = arr1[i][1] === arr2[j][1]
+            if(expresion) {
+                newArr.push(arr2[j])
+            } 
         }
     }
+    
     console.log(newArr)
 }
 
-var curInv = [
+let curInv = [
     [21, "Bowling Ball"],
     [2, "Dirty Sock"],
     [1, "Hair Pin"],
     [5, "Microphone"]
 ];
 
-var newInv = [
+let newInv = [
     [2, "Hair Pin"],
     [3, "Half-Eaten Apple"],
     [67, "Bowling Ball"],
